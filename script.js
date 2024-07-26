681291ec684116bc1953e7d204299ab19756594e
josueramirez1 <josue67ramirez@gmail.com>
2024-07-26 02:12:41 -0700

    creating functionality for adding values to table
    provides user the opportunity to type information and add it to list as well as using remove button if they are finished reading the book.

--- script.js
+++ script.js
@@ -3,13 +3,29 @@ const container = document.querySelector(".container");
 const addBookBtn = document.querySelector(".add-book-btn");
 const modal = document.querySelector(".modal");
 const form = document.querySelector("form");
+const table = document.querySelector("table");
 
+// Make modal appear and disappear
 addBookBtn.addEventListener("click", (e) => {
-  modal.style.display = "block";
+  modal.classList.add("show");
 });
 
 modal.addEventListener("click", (e) => {
-  if (e.target === modal) modal.style.display = "none";
+  if (e.target === modal) modal.classList.remove("show");
+});
+
+// form
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // Select all inputs
+  let title = document.querySelector("input[id='title']").value;
+  let author = document.querySelector("input[id='author']").value;
+  let pages = parseInt(document.querySelector("input[id='pages']").value);
+  let read = document.querySelector("input[id='read']").checked;
+  // Send values to constructor function
+  let book = new Book(author, title, pages, read);
+
+  addBookToLibrary(book);
 });
 
 // CONSTRUCTOR FUNCTION TO CREATE BOOK
@@ -18,19 +34,20 @@ function Book(author, title, pages, read) {
   this.author = author;
   this.title = title;
   this.pages = pages;
-  this.read = false;
+  this.read = read;
 }
 
 // HELPER FUNCTIONS
 
-function addBookToLibrary(title, author, pages, read) {}
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  addBookToPage(myLibrary);
+}
 
-// function addBookToPage(arr) {
-//   if (!arr) return;
-//   let tr = document.createElement("tr");
-//   arr.forEach((book) => {
-//     let tr = document.createElement("tr");
-//     tr.innerHTML = `<td>${book.title}</td><td>${book.author}</td><td>${book.pages}</td><td>${book.read}</td>`;
-//     table.appendChild(tr);
-//   });
-// }
+function addBookToPage(arr) {
+  arr.forEach((book) => {
+    let tr = document.createElement("tr");
+    tr.innerHTML = `<td>${book.title}</td><td>${book.author}</td><td>${book.pages}</td><td>${book.read}</td><button class="remove">Remove</button>`;
+    table.appendChild(tr);
+  });
+}
