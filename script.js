5febb67ef308cf71311520aa3e4774014e3d7f6e
josueramirez1 <josue67ramirez@gmail.com>
2024-07-26 14:30:59 -0700

    Clearing values everytime user clicks to add a new book
    It provides users the ability to add new book without having to manually delete

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const myLibrary = [];
-const container = document.querySelector(".container");
-const addBookBtn = document.querySelector(".add-book-btn");
-const modal = document.querySelector(".modal");
 const form = document.querySelector("form");
 const table = document.querySelector("table");
+const modal = document.querySelector(".modal");
+const inputs = [...document.querySelectorAll("input[type='text']")];
+const read = [...document.querySelectorAll("input[id='read']")];
 
-// Make modal appear and disappear
-addBookBtn.addEventListener("click", (e) => {
-  modal.classList.add("show");
-});
-
-modal.addEventListener("click", (e) => {
-  if (e.target === modal) modal.classList.remove("show");
+document.addEventListener("click", (e) => {
+  // Make modal appear and disappear
+  if (e.target.matches(".add-book-btn")) {
+    modal.classList.add("show");
+    for (let userInput of inputs) {
+      userInput.value = "";
+    }
+    read.forEach((mark) => {
+      mark.checked = false;
+    });
+  }
+  if (e.target.matches(".modal")) modal.classList.remove("show");
 });
 
 // form
@@ -21,11 +26,22 @@ form.addEventListener("submit", (e) => {
   let title = document.querySelector("input[id='title']").value;
   let author = document.querySelector("input[id='author']").value;
   let pages = parseInt(document.querySelector("input[id='pages']").value);
-  let read = document.querySelector("input[id='read']").checked;
-  // Send values to constructor function
-  let book = new Book(author, title, pages, read);
 
+  // variable to check if user chose a radio button
+  const noChoice = read.map((mark) => mark.checked);
+  if (!title || !author || !pages || noChoice.forEach((btn) => btn === false))
+    return;
+  // If user chose a radio button, find value of chosen radio button
+  const choice = read.find((mark) => {
+    if (mark.checked) {
+      return mark.value;
+    }
+  });
+  // Use variable to create new object and add it to the library
+  let book = new Book(author, title, pages, choice.value);
   addBookToLibrary(book);
+  // Remove modal once user submits response
+  modal.classList.remove("show");
 });
 
 // CONSTRUCTOR FUNCTION TO CREATE BOOK
@@ -41,13 +57,11 @@ function Book(author, title, pages, read) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
-  addBookToPage(myLibrary);
+  addBookToPage(book);
 }
 
-function addBookToPage(arr) {
-  arr.forEach((book) => {
-    let tr = document.createElement("tr");
-    tr.innerHTML = `<td>${book.title}</td><td>${book.author}</td><td>${book.pages}</td><td>${book.read}</td><button class="remove">Remove</button>`;
-    table.appendChild(tr);
-  });
+function addBookToPage(book) {
+  let tr = document.createElement("tr");
+  tr.innerHTML = `<td>${book.title}</td><td>${book.author}</td><td>${book.pages}</td><td>${book.read}</td><button class="remove">Remove</button>`;
+  table.appendChild(tr);
 }
