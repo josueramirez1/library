f2df0e6ddf96c422afab2de366ebe6a488051b21
josueramirez1 <josue67ramirez@gmail.com>
2024-08-15 16:06:57 -0700

    putting event listeners inside function
    removes global variables and adds them inside private function for security

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-const form = document.querySelector("form");
-const modal = document.querySelector(".modal");
-const textInputs = [...document.querySelectorAll("input[type='text']")];
-const numberInputs = [...document.querySelectorAll("input[type='number']")];
-const inputs = [...textInputs, ...numberInputs];
-const read = [...document.querySelectorAll("input[id='read']")];
-let book;
-
 // CONSTRUCTOR FUNCTION TO CREATE BOOK
 class Book {
   constructor(title, author, pages, read) {
@@ -64,52 +56,66 @@ function addBookToPage(book) {
   tr.appendChild(actionsTd);
 }
 
-// EVENT LISTENERS
-document.addEventListener("click", (e) => {
-  // Make modal appear and disappear
-  if (e.target.matches(".add-book-btn")) {
-    modal.classList.add("show");
-    for (let userInput of inputs) {
-      userInput.value = "";
+// Function to begin event listeners
+
+function startEvents() {
+  const form = document.querySelector("form");
+  const modal = document.querySelector(".modal");
+  const textInputs = [...document.querySelectorAll("input[type='text']")];
+  const numberInputs = [...document.querySelectorAll("input[type='number']")];
+  const inputs = [...textInputs, ...numberInputs];
+  const read = [...document.querySelectorAll("input[id='read']")];
+  let book;
+
+  // EVENT LISTENERS
+  document.addEventListener("click", (e) => {
+    // Make modal appear and disappear
+    if (e.target.matches(".add-book-btn")) {
+      modal.classList.add("show");
+      for (let userInput of inputs) {
+        userInput.value = "";
+      }
+      read.forEach((mark) => {
+        mark.checked = false;
+      });
     }
-    read.forEach((mark) => {
-      mark.checked = false;
-    });
-  }
-  if (e.target.matches(".modal")) modal.classList.remove("show");
+    if (e.target.matches(".modal")) modal.classList.remove("show");
 
-  if (e.target.matches(".remove")) {
-    let row = e.target.closest(".row");
-    // let row = e.target.closest(".row").dataset.id;
-    row.remove();
-  }
+    if (e.target.matches(".remove")) {
+      let row = e.target.closest(".row");
+      // let row = e.target.closest(".row").dataset.id;
+      row.remove();
+    }
 
-  if (e.target.matches(".status")) {
-    book.changeStatus(e);
-  }
-});
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  // Select all inputs
-  let title = document.querySelector("input[id='title']").value;
-  let author = document.querySelector("input[id='author']").value;
-  let pages = parseInt(document.querySelector("input[id='pages']").value);
-
-  // variable to check if user chose a radio button
-  const noChoice = read.map((mark) => mark.checked);
-  if (!title || !author || !pages || noChoice.forEach((btn) => btn === false))
-    return;
-  // If user chose a radio button, find value of chosen radio button
-  const choice = read.find((mark) => {
-    if (mark.checked) {
-      return mark.value;
+    if (e.target.matches(".status")) {
+      book.changeStatus(e);
     }
   });
-  // Use variable to create new object and add it to the library
-  book = new Book(title, author, pages, choice.value);
 
-  addBookToLibrary(book);
-  // Remove modal once user submits response
-  modal.classList.remove("show");
-});
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // Select all inputs
+    let title = document.querySelector("input[id='title']").value;
+    let author = document.querySelector("input[id='author']").value;
+    let pages = parseInt(document.querySelector("input[id='pages']").value);
+
+    // variable to check if user chose a radio button
+    const noChoice = read.map((mark) => mark.checked);
+    if (!title || !author || !pages || noChoice.forEach((btn) => btn === false))
+      return;
+    // If user chose a radio button, find value of chosen radio button
+    const choice = read.find((mark) => {
+      if (mark.checked) {
+        return mark.value;
+      }
+    });
+    // Use variable to create new object and add it to the library
+    book = new Book(title, author, pages, choice.value);
+
+    addBookToLibrary(book);
+    // Remove modal once user submits response
+    modal.classList.remove("show");
+  });
+}
+
+const addBooks = startEvents();
