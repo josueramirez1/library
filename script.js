5f4c801e81b59ea9a852c0dfdacb537f2fa64b28
josueramirez1 <josue67ramirez@gmail.com>
2026-08-07 15:52:07 -0700

    adding table to library

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
+const myLibrary = [];
+const container = document.querySelector(".container");
+
 function Book(title, author, pages, isRead = false) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.isRead = isRead ? "completed" : "not read yet";
   this.info = function () {
-    console.log(
-      `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead}.`,
-    );
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead}.`;
   };
 }
 
-const book1 = new Book("Harry Potter", "J. R. Tolkien", 320, false);
+function addBookToLibrary(title, author, pages, isRead) {
+  if (arguments.length === 0) return;
+  myLibrary.push(new Book(title, author, pages, isRead));
+}
 
-book1.info();
+addBookToLibrary("Making Sense of God", "Timothy Keller", 500, false);
+addBookToLibrary("Reading the Bible for a Change", "Ray Lubeck", 300, false);
+
+function displayBooks() {
+  const table = document.createElement("table");
+  const row = document.createElement("tr");
+  const thTitle = document.createElement("th");
+  const thAuthor = document.createElement("th");
+  const thPages = document.createElement("th");
+  const thIsRead = document.createElement("th");
+
+  thTitle.textContent = "Title";
+  thAuthor.textContent = "Author";
+  thPages.textContent = "Pages";
+  thIsRead.textContent = "Completed?";
+
+  table.appendChild(row);
+  row.append(thTitle, thAuthor, thPages, thIsRead);
+
+  table.classList.add("table");
+  console.log(container);
+
+  myLibrary.forEach((book) => {
+    console.log(book);
+    const row = document.createElement("tr");
+    const trTitle = document.createElement("td");
+    trTitle.textContent = book.title;
+    row.appendChild(trTitle);
+    table.appendChild(row);
+  });
+
+  container.appendChild(table);
+}
+
+displayBooks();
